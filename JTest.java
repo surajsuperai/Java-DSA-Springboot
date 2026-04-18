755b5cb2f8a6bf8cd0b50e784eb4c6c05a38687f
surajsuperai <er.srk123@gmail.com>
2026-04-18 18:46:34 +0530

    changed structure

--- JTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class JTest {
-    public static void main(String[] args) {
-         // System.out.println(args[3]);
-         Scanner input = new Scanner(System.in);
-         System.out.println(input.nextInt());
-         System.out.println(input.next());
-    }
-}
