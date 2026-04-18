da30ab8396f2519b68085395365b24064bfca333
surajsuperai <er.srk123@gmail.com>
2026-04-18 17:46:01 +0530

    Add files via upload

---
+++ JTest.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class JTest {
+    public static void main(String[] args) {
+         // System.out.println(args[3]);
+         Scanner input = new Scanner(System.in);
+         System.out.println(input.nextInt());
+         System.out.println(input.next());
+    }
+}
